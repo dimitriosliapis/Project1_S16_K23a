caeea4761cc8a93b682f189ab538e239264210f4
dimitriosliapis <sdi1100038@di.uoa.gr>
2016-11-02 12:47:18 +0200

    main.c -> μετρητης χρονου εκτελεσης

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <sys/time.h>
 #include "index.h"
 #include "string.h"
 
@@ -28,6 +29,11 @@ int main(int argc, char *argv[]) {
         return 0;
     }
 
+    ///////////////////////////////////////////////////////////////////////////////
+    struct timeval  tv1, tv2;
+    gettimeofday(&tv1, NULL);
+    ///////////////////////////////////////////////////////////////////////////////
+
     char str[64];
     fgets(str, sizeof(str), Graph);
 
@@ -44,7 +50,13 @@ int main(int argc, char *argv[]) {
 
         fgets(str, sizeof(str), Graph);
     }
+///////////////////////////////////////////////////////////////////////////////
+    gettimeofday(&tv2, NULL);
 
+    printf ("Total time = %f seconds\n",
+            (double) (tv2.tv_usec - tv1.tv_usec) / 1000000 +
+            (double) (tv2.tv_sec - tv1.tv_sec));
+///////////////////////////////////////////////////////////////////////////////
     return 0;
 }
 
