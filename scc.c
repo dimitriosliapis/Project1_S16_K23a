84e6e2fb7a25581386effdd42f4bbcd9eeee364d
AlexGavalas <sdi1000004@di.uoa.gr>
2016-11-29 16:38:17 +0200

    scc

--- scc.c
+++ scc.c
@@ -25,11 +25,12 @@ void tarjan(ind *index_out, list_node *buffer_out, uint32_t size_out) {
         if(search(explored, i, HT_BIG) == FOUND) continue;
 
         push(&stack, i);
-        index_out[i].lowlink = index;
-        index_out[i].index = index;
-        index++;
         while(!stackIsEmpty(&stack)) {
             v = peek(&stack);
+
+            index_out[v].lowlink = index;
+            index_out[v].index = index;
+            index++;
 
             if(search(explored, v, HT_BIG) == NOT_FOUND) {
                 insert(explored, v, HT_BIG);
@@ -40,7 +41,9 @@ void tarjan(ind *index_out, list_node *buffer_out, uint32_t size_out) {
                     k = 0;
                     while(k < N) {
                         if(neighbors_out->neighbor[k] == DEFAULT) break;
-                        push(&stack, neighbors_out->neighbor[k]);
+                        if(search(explored, neighbors_out->neighbor[k], HT_BIG) == NOT_FOUND) {
+                            push(&stack, neighbors_out->neighbor[k]);
+                        }
                         k++;
                         if(k == N && neighbors_out->nextListNode != -1) {
                             neighbors_out = buffer_out + neighbors_out->nextListNode;
@@ -48,15 +51,17 @@ void tarjan(ind *index_out, list_node *buffer_out, uint32_t size_out) {
                         }
                     }
                 }
-                else if(index_out[v].lowlink == index_out[v].index) {
-                    scc->components[i].component_id = i;
-                    while (k != v) {
-                        k = pop(&stack);
-                        scc->components[i].included_node_ids[a] = k;
-                        scc->components[i].included_nodes_count++;
-                        a++;
+                else  {
+                    if (index_out[v].lowlink == index_out[v].index) {
+                        scc->components[i].component_id = i;
+                        while (k != v) {
+                            k = pop(&stack);
+                            scc->components[i].included_node_ids[a] = k;
+                            scc->components[i].included_nodes_count++;
+                            a++;
+                        }
+                        a = 0;
                     }
-                    a = 0;
                 }
             }
         }
