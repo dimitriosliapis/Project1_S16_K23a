a4ec39f341104667a6054fa417ea41a664af0aff
dimitriosliapis <sdi1100038@di.uoa.gr>
2016-12-03 20:33:37 +0200

    tarjan new

--- scc.c
+++ scc.c
@@ -67,21 +67,23 @@ SCC* estimateStronglyConnectedComponents(ind *index_out, list_node *buffer_out, 
 
 SCC* tarjan(ind *index_out, list_node *buffer_out, uint32_t size_out, uint32_t num_nodes) {
 
-    Stack stack;
-    Stack parent_stack;
+    Stack scc_stack;
+    Stack dfs_stack;
     ht_Node *explored = createHashtable(HT_BIG);
-    ht_Node *explored_stack = createHashtable(HT_BIG);
+    ht_Node *explored_twice = createHashtable(HT_BIG);
+    ht_Node *explored_scc = createHashtable(HT_BIG);
     list_node *neighbors_out;
     ptrdiff_t offset_out;
     uint32_t size = size_out, index = 0, realloc_node_size;
-    uint32_t i = 0, v = 0, k = 0, a = 0, check = 0;
+    uint32_t i = 0, v = 0, k = 0, a = 0, w = 0, caller = DEFAULT;
     uint32_t scc_counter = 0;
+    int all_child_in_scc = 0;
 
     SCC *scc = malloc(sizeof(SCC));
 
     scc->components = malloc(COMPONENT_SIZE*sizeof(Component));
     //for(k = 0 ; k < COMPONENT_SIZE ; k++) scc->components[k] = NULL;
-    scc->components_count = DEFAULT;
+    scc->components_count = 0;
 
     scc->id_belongs_to_component = malloc(num_nodes*sizeof(uint32_t));
     for(i = 0; i < num_nodes; i++){
@@ -89,124 +91,149 @@ SCC* tarjan(ind *index_out, list_node *buffer_out, uint32_t size_out, uint32_t n
     }
 
 
-    stack.last = NULL;
-    parent_stack.last = NULL;
+    scc_stack.last = NULL;
+    dfs_stack.last = NULL;
+
 
     for(i = 0 ; i < size ; i++) {
         if(lookup(index_out, i, size_out) == NOT_EXIST) continue;
-        if(search(explored, i, HT_BIG) == FOUND) continue;
+        if(search(explored_scc, i, HT_BIG) == FOUND) continue;
 
-        push(&stack, i);
-        index_out[i].lowlink = index;
-        index_out[i].index = index;
-        index++;
+        caller = DEFAULT;
 
-        while(!stackIsEmpty(&stack)) {
-            if(check == 0) {
-                v = peek(&stack);
-                if(v == DEFAULT) return NULL;
+        index = 1;
+
+        push(&dfs_stack, i);
+        push(&scc_stack, i);
+
+        while(!stackIsEmpty(&scc_stack)){
+
+            v = peek(&dfs_stack);
+            if(search(explored, v, HT_BIG) == NOT_FOUND) {
+                index_out[v].index = index;
+                index_out[v].lowlink = index;
+                index++;
+                insert(explored, v, HT_BIG);
             }
-            else {
-                v = nextfromStack(&parent_stack, v);
-                if(v == DEFAULT) return NULL;
-            }
-            check = 0;
 
-            if(onStack(&parent_stack, v)) {
 
-                offset_out = getListHead(index_out, v);
-                neighbors_out = buffer_out + offset_out;
+            offset_out = getListHead(index_out, v);
+            neighbors_out = buffer_out + offset_out;
 
+            if(offset_out >= 0 && neighbors_out->neighbor[0] != DEFAULT && all_child_in_scc == 0){   //if v has children
+
+                if(search(explored_twice, v, HT_BIG) == FOUND) {
+                    pop(&dfs_stack);
+                }
                 k = 0;
                 while (k < N) {
-                    if(neighbors_out->neighbor[k] == DEFAULT) break;
-                    if(index_out[v].lowlink > index_out[neighbors_out->neighbor[k]].lowlink) {
-                        index_out[v].lowlink = index_out[neighbors_out->neighbor[k]].lowlink;
+                    w = neighbors_out->neighbor[k];
+                    if(w == DEFAULT) break;
+
+                    if(search(explored_scc, w, HT_BIG) == FOUND){
+                        k++;
+                        all_child_in_scc = 1;
+                        continue;
                     }
-                    else if(onStack(&stack, k)) {
-                        if(index_out[v].lowlink > index_out[neighbors_out->neighbor[k]].index) index_out[v].lowlink = index_out[neighbors_out->neighbor[k]].index;
+                    all_child_in_scc = 0;
+                    if(search(explored, w, HT_BIG) == NOT_FOUND){
+                        index_out[w].index = index;
+                        index_out[w].lowlink = index;
+                        index++;
+                        push(&scc_stack, w);
+                        push(&dfs_stack, w);
+                        insert(explored, w, HT_BIG);
+
+                    }
+                    else{
+                        if(search(explored_twice, w, HT_BIG) == NOT_FOUND){
+
+                            if (index_out[v].lowlink > index_out[w].lowlink)
+                                index_out[v].lowlink = index_out[w].lowlink;
+
+                            push(&dfs_stack, w);
+                            caller = v;
+                            insert(explored_twice, w, HT_BIG);
+                        }
+                        else{
+
+                            if(index_out[v].lowlink > index_out[w].index) index_out[v].lowlink = index_out[w].index;
+
+                            scc->components[scc_counter].component_id = scc_counter;
+                            scc->components[scc_counter].included_nodes_count = 0;
+                            scc->components[scc_counter].node_array_size = NODE_IDS_SIZE;
+                            scc->components[scc_counter].included_node_ids = malloc(scc->components[scc_counter].node_array_size*sizeof(uint32_t));
+                            for(a = 0 ; a < scc->components[scc_counter].node_array_size ; a++) scc->components[scc_counter].included_node_ids[a] = DEFAULT;
+                            a = 0;
+
+
+                            v = pop(&dfs_stack);
+                            while(index_out[v].index != index_out[v].lowlink) v = pop(&dfs_stack);
+                            while(1){
+                                w = pop(&scc_stack);
+                                if(w == DEFAULT) break;
+                                insert(explored_scc, w, HT_BIG);
+
+                                scc->components[scc_counter].included_node_ids[a] = w;
+
+                                if(a == (scc->components[scc_counter].node_array_size-1)) {
+                                    realloc_node_size = 2*scc->components[scc_counter].node_array_size;
+                                    scc->components[scc_counter].included_node_ids = realloc(scc->components[scc_counter].included_node_ids, realloc_node_size*sizeof(uint32_t));
+                                    for(a = scc->components[scc_counter].node_array_size ; a < realloc_node_size ; a++) scc->components[scc_counter].included_node_ids[a] = DEFAULT;
+                                    scc->components[scc_counter].node_array_size = realloc_node_size;
+                                }
+                                scc->components[scc_counter].included_nodes_count++;
+                                scc->id_belongs_to_component[w] = scc_counter;
+
+                                a++;
+                                printf("ta scc %d exei ta %d ids\n", scc_counter, w);
+                                if(w == v) break;
+                            }
+                            scc_counter++;
+                        }
                     }
                     k++;
-                    if(k == N && neighbors_out->nextListNode != -1) {
+                    if(k >= N && neighbors_out->nextListNode != -1) {
                         neighbors_out = buffer_out + neighbors_out->nextListNode;
                         k = 0;
                     }
                 }
+
             }
+            else{
+                w = pop(&scc_stack);
+                pop(&dfs_stack);
 
-            if(search(explored, v, HT_BIG) == NOT_FOUND || onStack(&parent_stack, v)) {
-                if(search(explored, v, HT_BIG) == NOT_FOUND) insert(explored, v, HT_BIG);
+                insert(explored, w, HT_BIG);
+                insert(explored_scc, w, HT_BIG);
 
-                offset_out = getListHead(index_out, v);
-                neighbors_out = buffer_out + offset_out;
+                scc->components[scc_counter].component_id = scc_counter;
+                scc->components[scc_counter].included_nodes_count = 0;
+                scc->components[scc_counter].node_array_size = NODE_IDS_SIZE;
+                scc->components[scc_counter].included_node_ids = malloc(scc->components[scc_counter].node_array_size*sizeof(uint32_t));
+                for(a = 0 ; a < scc->components[scc_counter].node_array_size ; a++) scc->components[scc_counter].included_node_ids[a] = DEFAULT;
+                a = 0;
 
-                if(offset_out >= 0 && neighbors_out->neighbor[0] != DEFAULT && !onStack(&parent_stack, v)) { // den htan swsti sinthiki gia na deis an exei neighbors
-                    push(&parent_stack, v);
-                    k = 0;
+                scc->components[scc_counter].included_node_ids[a] = w;
 
-                    while(k < N) {
-                        if(neighbors_out->neighbor[k] == DEFAULT) break;
-                        if(search(explored, neighbors_out->neighbor[k], HT_BIG) == NOT_FOUND) {
-                            insert(explored, neighbors_out->neighbor[k], HT_BIG);
-                            push(&stack, neighbors_out->neighbor[k]);
-                            index_out[neighbors_out->neighbor[k]].lowlink = index;
-                            index_out[neighbors_out->neighbor[k]].index = index;
-                            index++;
-                        }
-                        k++;
-                        if(k == N && neighbors_out->nextListNode != -1) {
-                            neighbors_out = buffer_out + neighbors_out->nextListNode;
-                            k = 0;
-                        }
-                    }
+                if(a == (scc->components[scc_counter].node_array_size-1)) {
+                    realloc_node_size = 2*scc->components[scc_counter].node_array_size;
+                    scc->components[scc_counter].included_node_ids = realloc(scc->components[scc_counter].included_node_ids, realloc_node_size*sizeof(uint32_t));
+                    for(a = scc->components[scc_counter].node_array_size ; a < realloc_node_size ; a++) scc->components[scc_counter].included_node_ids[a] = DEFAULT;
+                    scc->components[scc_counter].node_array_size = realloc_node_size;
                 }
-                else if(index_out[v].lowlink == index_out[v].index) {
-                    scc->components[scc_counter].component_id = scc_counter;
-                    scc->components[scc_counter].included_nodes_count = 0;
-                    scc->components[scc_counter].node_array_size = NODE_IDS_SIZE;
-                    scc->components[scc_counter].included_node_ids = malloc(scc->components[scc_counter].node_array_size*sizeof(uint32_t));
-                    for(a = 0 ; a < scc->components[scc_counter].node_array_size ; a++) scc->components[scc_counter].included_node_ids[a] = DEFAULT;
-                    k = DEFAULT;
-                    a = 0;
-                    while (k != v) {
-                        k = pop(&stack);
-                        if(k == DEFAULT) break;
-                        if(search(explored_stack, k, HT_BIG) == FOUND) continue;
-                        insert(explored_stack, k, HT_BIG);
-                        if(onStack(&parent_stack, k)) removefromStack(&parent_stack, k);
+                scc->components[scc_counter].included_nodes_count++;
+                scc->id_belongs_to_component[w] = scc_counter;
 
-                        scc->components[scc_counter].included_node_ids[a] = k;
-
-                        if(a == (scc->components[scc_counter].node_array_size-1)) {
-                            realloc_node_size = 2*scc->components[scc_counter].node_array_size;
-                            scc->components[scc_counter].included_node_ids = realloc(scc->components[scc_counter].included_node_ids, realloc_node_size*sizeof(uint32_t));
-                            for(a = scc->components[scc_counter].node_array_size ; a < realloc_node_size ; a++) scc->components[scc_counter].included_node_ids[a] = DEFAULT;
-                            scc->components[scc_counter].node_array_size = realloc_node_size;
-                        }
-                        scc->components[scc_counter].included_nodes_count++;
-                        scc->id_belongs_to_component[k] = scc_counter;
-
-                        a++;
-                        printf("ta scc %d exei ta %d ids\n", scc_counter, k);
-                    }
-
-                    scc_counter++;
-                }
-                else {
-                    pop(&parent_stack);
-                    v = peek(&parent_stack);
-                    if(v == DEFAULT) return NULL;
-                    check = 1;
-                }
+                a++;
+                printf("ta scc %d exei ta %d ids\n", scc_counter, w);
+                scc_counter++;
             }
-
         }
     }
-
     scc->components_count = scc_counter;
-
     delete(explored, HT_BIG);
-    delete(explored_stack, HT_BIG);
+    delete(explored_scc, HT_BIG);
 
 
 
