c5f7b46a3c2f05e70bacf5bc0648307dbfaf7940
AlexGavalas <sdi1000004@di.uoa.gr>
2016-12-06 13:03:32 +0200

    hash me versions

--- hash.h
+++ hash.h
@@ -9,8 +9,14 @@
 #define FOUND 1
 #define NOT_FOUND -1
 
+typedef struct bucket_t{
+    uint32_t id;
+    uint32_t version;
+}node_bucket;
+
 typedef struct htNode_t {
-    uint32_t *bucket;
+    node_bucket *bucket;
+    //uint32_t *bucket;
     uint32_t size;
 } ht_Node;
 
@@ -18,10 +24,10 @@ unsigned int hash(uint32_t x);
 
 ht_Node *createHashtable(uint32_t);
 
-int search(ht_Node *, uint32_t, uint32_t);
+int search(ht_Node *, uint32_t, uint32_t, uint32_t);
 
-void insert(ht_Node *, uint32_t, uint32_t);
+void insert(ht_Node *, uint32_t, uint32_t, uint32_t);
 
-void reinitialize(ht_Node *hashTable, uint32_t size);
+//void reinitialize(ht_Node *hashTable, uint32_t size);
 
 void delete(ht_Node *, uint32_t);
