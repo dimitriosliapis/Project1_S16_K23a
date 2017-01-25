5b04d33da19e4de59ad586fe3a372a6276204488
AlexGavalas <sdi1000004@di.uoa.gr>
2017-01-26 01:58:53 +0200

    apopeira epanaliptikou

--- scc.c
+++ scc.c
@@ -186,7 +186,7 @@ SCC *estimateStronglyConnectedComponents_iterative(ind *index_out, list_node *bu
 
 void tarjan_iterative(SCC **scc, ind *index_out, list_node *buffer_out, uint32_t v, uint32_t index, Stack_t *scc_stack, uint32_t *neigh_counter, uint32_t *caller, uint32_t *list_node_counter) {
 
-    uint32_t w, last, new_last, scc_counter, a, realloc_node_size, i, flag = 0, end = 0;
+    uint32_t w, last, new_last, scc_counter, a, realloc_node_size, i, flag = 0, end = 0, k = 0;
     ptrdiff_t offset_out;
     list_node *neighbors_out;
 
@@ -204,121 +204,91 @@ void tarjan_iterative(SCC **scc, ind *index_out, list_node *buffer_out, uint32_t
 
     while(1) {
 
-        if(index_out[last].children_in_scc < index_out[last].num_of_children) {
+        last = peekfromstack(scc_stack);
+
+        if (index_out[last].children_in_scc < index_out[last].num_of_children) {
 
             offset_out = getListHead(index_out, last);
             neighbors_out = buffer_out + offset_out;
 
-            if(neigh_counter[last] == N) {
-                for(i = 0 ; i <= list_node_counter[last] ; i++) {
-                    if(neighbors_out->nextListNode != -1) neighbors_out = neighbors_out + neighbors_out->nextListNode;
-                    else {
-                        flag = 1;
-                        last = caller[last];
-                        if(last != DEFAULT) break;
-                        else {
-                            end = 1;
-                            break;
-                        }
+            k = 0;
+
+            while (k < N) {
+
+                w = neighbors_out->neighbor[k];
+
+                if (w == DEFAULT) break;
+
+                if (index_out[w].index == DEFAULT) {
+
+                    caller[w] = last;
+
+                    index_out[w].children_in_scc = 0;
+                    index_out[w].index = index;
+                    index_out[w].lowlink = index;
+                    index++;
+
+                    pushinstack(scc_stack, w);
+                    index_out[w].onStack = 1;
+                } else if (index_out[w].onStack == 1) {
+                    if (index_out[last].lowlink > index_out[w].index) index_out[last].lowlink = index_out[w].index;
+                }
+
+                k++;
+                if (k == N && neighbors_out->nextListNode != -1) {
+                    neighbors_out = buffer_out + neighbors_out->nextListNode;
+                    k = 0;
+                }
+            }
+        } else {
+            if (index_out[last].lowlink == index_out[last].index) {
+
+                scc_counter = (*scc)->components_count;
+                (*scc)->components[scc_counter].included_nodes_count = 0;
+                (*scc)->components[scc_counter].node_array_size = NODE_IDS_SIZE;
+                (*scc)->components[scc_counter].included_node_ids = malloc(NODE_IDS_SIZE * sizeof(uint32_t));
+                (*scc)->components[scc_counter].included_node_ids[0] = DEFAULT;
+
+                a = 0;
+
+                //ftiakse to SCC
+                do {
+                    w = popfromstack(scc_stack);
+                    index_out[w].onStack = 0;
+
+                    (*scc)->components[scc_counter].included_node_ids[a] = w;
+
+                    if (a == ((*scc)->components[scc_counter].node_array_size - 1)) {
+                        realloc_node_size = 2 * (*scc)->components[scc_counter].node_array_size;
+                        (*scc)->components[scc_counter].included_node_ids = realloc(
+                                (*scc)->components[scc_counter].included_node_ids,
+                                realloc_node_size * sizeof(uint32_t));
+                        (*scc)->components[scc_counter].node_array_size = realloc_node_size;
                     }
+
+                    (*scc)->components[scc_counter].included_node_ids[a + 1] = DEFAULT;
+                    (*scc)->components[scc_counter].included_nodes_count++;
+                    (*scc)->id_belongs_to_component[w] = scc_counter;
+
+                    a++;
+
+                } while (w != last);
+
+                (*scc)->components_count++;//gia to epomeno SCC
+            }
+
+            new_last = caller[last];
+
+            if (new_last != DEFAULT) {
+                if (index_out[new_last].lowlink > index_out[last].lowlink) {
+                    index_out[new_last].lowlink = index_out[last].lowlink;
                 }
-                if(end != 1) {
-                    list_node_counter[last]++;
-                    neigh_counter[last] = 0;
-                }
-            }
-
-            if(end == 1) break;
-
-            if(flag == 1) {
-                flag = 0;
-                continue;
-            }
-
-            w = neighbors_out->neighbor[neigh_counter[last]];
-            neigh_counter[last]++;
-
-            if(w == DEFAULT) {
-                last = caller[last];
-                if(last != DEFAULT) continue;
-                else break;
-            }
-
-            index_out[last].children_in_scc++;
-
-            if(index_out[w].index == DEFAULT) {
-
-                caller[w] = last;
-
-                index_out[w].children_in_scc = 0;
-                index_out[w].index = index;
-                index_out[w].lowlink = index;
-                index++;
-
-                pushinstack(scc_stack, w);
-                index_out[w].onStack = 1;
-
-                last = w;
-            }
-
-            else if(index_out[w].onStack == 1) {
-                if (index_out[last].lowlink > index_out[w].index) index_out[last].lowlink = index_out[w].index;
-            }
-        }
-
-        else if (index_out[last].lowlink == index_out[last].index && index_out[last].lowlink != DEFAULT) {
-
-            scc_counter = (*scc)->components_count;
-            (*scc)->components[scc_counter].included_nodes_count = 0;
-            (*scc)->components[scc_counter].node_array_size = NODE_IDS_SIZE;
-            (*scc)->components[scc_counter].included_node_ids = malloc(NODE_IDS_SIZE * sizeof(uint32_t));
-            (*scc)->components[scc_counter].included_node_ids[0] = DEFAULT;
-
-            a = 0;
-
-            //ftiakse to SCC
-            do {
-                w = popfromstack(scc_stack);
-                index_out[w].onStack = 0;
-                caller[w] = DEFAULT;
-
-                (*scc)->components[scc_counter].included_node_ids[a] = w;
-
-                if (a == ((*scc)->components[scc_counter].node_array_size - 1)) {
-                    realloc_node_size = 2 * (*scc)->components[scc_counter].node_array_size;
-                    (*scc)->components[scc_counter].included_node_ids = realloc(
-                            (*scc)->components[scc_counter].included_node_ids,
-                            realloc_node_size * sizeof(uint32_t));
-                    (*scc)->components[scc_counter].node_array_size = realloc_node_size;
-                }
-
-                (*scc)->components[scc_counter].included_node_ids[a + 1] = DEFAULT;
-                (*scc)->components[scc_counter].included_nodes_count++;
-                (*scc)->id_belongs_to_component[w] = scc_counter;
-
-                a++;
-
-
-            } while (w != last);
-
-            (*scc)->components_count++;//gia to epomeno SCC
-        }
-
-        else {
-            if(last != DEFAULT) {
-                new_last = caller[last];
-
-                if (new_last != DEFAULT) {
-                    if (index_out[new_last].lowlink > index_out[last].lowlink) {
-                        index_out[new_last].lowlink = index_out[last].lowlink;
-                    }
-                    last = new_last;
-                }
-                else break;
+                last = new_last;
             }
             else break;
         }
     }
+
 }
 
 //free SCC
